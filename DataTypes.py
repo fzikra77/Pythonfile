952aa0d4d2d9f7bf1698007bebed60f7d662df99
Zikra <zikrayoung@gmail.com>
2025-05-25 17:59:47 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,12 @@
+name="Zikra"
+age=15
+is_student=True
+weight=40
+print("the value of name is:", name)
+print("Data type of variable name is:", type(name))
+print("The value of age is:", age)
+print("Data type of variable age is:", type(age))
+print("the value ofis_student is:", is_student)
+print("Data type of is_student name is:", type(is_student))
+print("The value of weight is:", weight)
+print("Data type of variable weight is:",type(weight))
