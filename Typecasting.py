452c16afd71ae2d9cebd1efc73d4413e62753767
Zikra <zikrayoung@gmail.com>
2025-05-25 18:18:44 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,20 @@
+name="Zikra"
+age=15
+is_student=True
+weight=40
+print("the value of name is:", name)
+print("Data type of variable name is:", type(name))
+print("The value of age is:", age)
+print("Data type of variable age is:", type(age))
+print("the value ofis_student is:", is_student)
+print("Data type of is_student name is:", type(is_student))
+print("The value of weight is:", weight)
+print("Data type of variable weight is:",type(weight))
+# Type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
